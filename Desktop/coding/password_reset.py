3b57c3ddbdd0428d7187e04e15efac46468c38aa
Cathy Chen <ingridlosna@gmail.com>
2021-02-08 23:11:14 +0800

    teacher's version

--- Desktop/coding/password_reset.py
+++ Desktop/coding/password_reset.py
@@ -1,15 +1,10 @@
 password = 'a123456'
-i = 0
-while True:
+i = 3 #剩餘機會
+while i > 0:
     user = input('請輸入密碼： ')
-    if user != password:
-        i += 1
-        if i == 1:
-            print('密碼錯誤！還有兩次機會')
-        elif i == 2:
-            print('密碼錯誤！還有一次機會')
-        else:
-            break
-    else:
-        print('登入成功！')
+    if user == password:
+        print('登入成功！') #逃出迴圈
         break
+    else:
+        i -= 1
+        print('密碼錯誤！還有', i, '次機會')
